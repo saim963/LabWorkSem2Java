74a7deb247c3b9e344ec4819e24fa3b560a796ef
Saim Shakeel <aims3328@gmail.com>
2024-02-12 20:43:12 +0530

    updates

--- w2q2_MyNum.java
+++ w2q2_MyNum.java
@@ -10,12 +10,15 @@ public class w2q2_MyNum {
         if(value<0) return (int) (value-1);
         return (int)value;
     }
+    static int round(double value){
+        return floor(value + 0.5d);
+    }
 
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
         System.out.print("Enter a number to perform operations: ");
         double num = sc.nextDouble();
-        System.out.println(Math.round(num));
+        System.out.println(round(num));
         // System.out.println(ceil(num));
         // System.out.println(floor(num));
         sc.close();
